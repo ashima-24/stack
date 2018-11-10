9c9ee30114ecd28fd0e035718449ffef7569d370
ashima-24 <jindal241995@gmail.com>
2018-11-11 00:17:33 +0100

    pointer

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
-	cout<<"Hi from main()";
-	return 0;
+	cout<<"Hi from main\n ";
+int a;
+int *p;
+p=&a;
+a=5;
+cout<<"\n"<<p;
+cout<<"\n"<<&p;    
+cout<<*p<<\n;
+    return 0;
 }
