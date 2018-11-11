3fae2817662f806c9172a7d898dce40a8eabb18f
ashima-24 <jindal241995@gmail.com>
2018-11-11 01:11:09 +0100

    2d

--- main.c
+++ main.c
@@ -2,12 +2,18 @@
 
 int main() {
 	printf("hello\n");
-
+/*
 int a[5]={1,2,3,4};
 printf("%ls\n",&a[0]) ;
 printf("%d\n",a[0]);
 printf("%d\n",*a);
-    
-return 0;
+  */
+    int a[1][2]={{1,2},{3,4}};
+
+    printf("%ls",*(a)  );
+    printf("%ls", *(a+1) );
+    printf("%ls", (*(a+1)+1) );
+//printf("%d",    )  
+    return 0;
 
 }
