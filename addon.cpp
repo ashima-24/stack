9e59542813a37d55042c3b84368cbc7782b8fd3a
ashima-24 <jindal241995@gmail.com>
2018-11-14 00:06:12 +0100

    add 1 on no

--- addon.cpp
+++ addon.cpp
@@ -4,12 +4,31 @@ using namespace std;
 
 int main()
 
-{
-    std:: vector<int>a={1,2,3,4};
+{ 
+    std:: vector<int>a={0};
+//cout<<b<<"\n";
 int b=a.size();
 std::cout<<b<<"\n";
-//cout<<b<<"\n";
-a[b-1]=a[b-1]+1;
+    a[b-1]=a[b-1]+1;
+
+if(a[b-1]==10)
+{
+a[b-1]=0;
+for(int i=b-2;i>=0;i--)
+{
+    a[i]+=1;
+if(a[i]!=10)
+break;
+else{
+
+    a[i]=0;
+if (i==0)
+    a[i]=10;
+
+}
+}
+ 
+}
 for(int y=0;y<b;y++)
 std::cout<<a[y];
 
