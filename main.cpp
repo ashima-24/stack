ff7a4d59448d6fab84a4d03e9c3218eafef9c0ec
ashima-24 <jindal241995@gmail.com>
2018-11-13 21:44:04 +0100

    vector try

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-
+#include<vector>
 using namespace std;
-
 int main() {
 	cout<<"Hi from main\n ";
+    std::vector<int>b;
+    b.push_back(10);
+    
+    std::vector<int> a;
+/*while(i<5)
+{
+    std::cin>>val;
+a.push_back(val);
+i++;
+}
+for(i=0;i<5;i++)
+{
+    std::cout<<a[i]<<"\n";
+    std::cout<<a.size();
+}
+
 int a;
 int *p;
 p=&a;
@@ -11,5 +26,5 @@ a=5;
 cout<<"\n"<<p;
 cout<<"\n"<<&p;    
 cout<<*p<<\n;
-    return 0;
+  */  return 0;
 }
